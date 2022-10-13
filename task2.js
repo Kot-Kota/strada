63fb3a08c0e4fe036feae044be6116b8cc10b5b0
Kot-Kota <ses29091987@gmail.com>
2022-10-12 17:39:28 -0700

    created new task

---
+++ task2.js
@@ -0,0 +1,12 @@
+let JSname = promt("What is the “official” name of JavaScript?");
+if (JSname == "ECMAScript") {
+    alert("Right!");
+} else {
+  alert("You don't know? “ECMAScript”!");  
+}
+// let JSname = "piugoiuy";
+// if (JSname == "ECMAScript"){
+//     console.log("Right!");  
+// } else {
+//     console.log("You don't know? “ECMAScript”!");
+// };
