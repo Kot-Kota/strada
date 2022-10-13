3bf0303f70b81c27c81bd899df1fea77aec387c7
Kot-Kota <ses29091987@gmail.com>
2022-10-12 18:34:32 -0700

    task4

--- Tasks/task2.js
+++ Tasks/task2.js
@@ -1,3 +1,7 @@
+/*
+Using the if..else construct, write the code which asks: ‘What is the “official” name of JavaScript?’
+If the visitor enters “ECMAScript”, then output “Right!”, otherwise – output: “You don’t know? ECMAScript!”
+*/
 let JSname = promt("What is the “official” name of JavaScript?");
 if (JSname == "ECMAScript") {
     alert("Right!");
