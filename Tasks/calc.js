f73848b631253b755980c3a1184d96bb37116b1f
smokvyna02 <olenasmokvyna@gmail.com>
2022-10-13 22:22:17 -0700

    calculator refactoring

--- Tasks/calc.js
+++ Tasks/calc.js
@@ -1,15 +1,22 @@
 // сalc(‘add’, 1, 2) - возвращает 3
 // сalc(‘multi’, 1, 2) - возвращает 2
 // сalc(’subtract’, 3, 2) - возвращает 1
-let operator;
-function calc(operator, a,b) {
+let operator = "-";
+let a = 3;
+let b = 2;
+let result = calc(a, operator, b)
+// let operator = prompt('Enter operator ( either +, - or * ): ');
+// let a = parseFloat(prompt('Enter first number: '));
+// let b = parseFloat(prompt('Enter second number: '));
+
+
+function calc(a, operator, b) {
  if (operator == "+" ) {
     return a + b;
  }else if (operator == "-") {
-    return a - b;
+   return a - b;
  }else if (operator == "*") {
-    return a * b;
+   return a * b;
  }
 } 
-let result = calc("*", 4, 5);
-console.log(result)
+console.log(`${a} ${operator} ${b} = ${result}`);
