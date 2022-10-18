1424ed041d2a01f3106467cf31922d57cce8aa03
Kot-Kota <ses29091987@gmail.com>
2022-10-18 14:57:06 -0700

    loops

--- Tasks/calc.js
+++ Tasks/calc.js
@@ -8,19 +8,46 @@
 // let result = calc(a, operator, b)
 
 
-let a = parseFloat(prompt('Enter first number: '));
+// let a = parseFloat(prompt('Enter first number: '));
+// let operator = prompt('Enter operator ( either +, - or * ): ');
+// let b = parseFloat(prompt('Enter second number: '));
+// let result = calc(a, operator, b)
+
+
+// function calc(a, operator, b) {
+//  if (operator == "+" ) {
+//     return a + b;
+//  }else if (operator == "-") {
+//    return a - b;
+//  }else if (operator == "*") {
+//    return a * b;
+//  }
+// } 
+// alert(`${a} ${operator} ${b} = ${result}`);
+let firstNumber = parseFloat(prompt('Enter first number: '));
 let operator = prompt('Enter operator ( either +, - or * ): ');
-let b = parseFloat(prompt('Enter second number: '));
-let result = calc(a, operator, b)
+let secondNumber = parseFloat(prompt('Enter second number: '));
+let result;
 
+function calc() {
+	getfirstNumber();
+	getsecondNumber();
 
-function calc(a, operator, b) {
- if (operator == "+" ) {
-    return a + b;
- }else if (operator == "-") {
-   return a - b;
- }else if (operator == "*") {
-   return a * b;
- }
-} 
-alert(`${a} ${operator} ${b} = ${result}`);
+switch (operator) {
+   case "+":
+      result = firstNumber + secondNumber;
+      alert(`Результат равен ${result}`);
+      break;
+   case "-":
+      result = firstNumber - secondNumber;
+      alert(`Результат равен ${result}`);
+      break;
+   case "*":
+      result = firstNumber * secondNumber;
+      alert(`Результат равен ${result}`);
+      break;
+      default:
+			alert(`Wrong characters!`);
+}
+}
+calc();
