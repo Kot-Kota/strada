9233206d2738985436c79c24ea1dc26c608850cd
Kot-Kota <ses29091987@gmail.com>
2022-10-18 20:25:18 -0700

    change

--- Tasks/calc.js
+++ Tasks/calc.js
@@ -1,53 +1,43 @@
-// сalc(‘add’, 1, 2) - возвращает 3
-// сalc(‘multi’, 1, 2) - возвращает 2
-// сalc(’subtract’, 3, 2) - возвращает 1
-
-// let operator = "-";
-// let a = 3;
-// let b = 2;
-// let result = calc(a, operator, b)
-
-
-// let a = parseFloat(prompt('Enter first number: '));
-// let operator = prompt('Enter operator ( either +, - or * ): ');
-// let b = parseFloat(prompt('Enter second number: '));
-// let result = calc(a, operator, b)
-
-
-// function calc(a, operator, b) {
-//  if (operator == "+" ) {
-//     return a + b;
-//  }else if (operator == "-") {
-//    return a - b;
-//  }else if (operator == "*") {
-//    return a * b;
-//  }
-// } 
-// alert(`${a} ${operator} ${b} = ${result}`);
-let firstNumber = parseFloat(prompt('Enter first number: '));
+let a = parseFloat(prompt('Enter first number: '));
 let operator = prompt('Enter operator ( either +, - or * ): ');
-let secondNumber = parseFloat(prompt('Enter second number: '));
-let result;
+let b = parseFloat(prompt('Enter second number: '));
+let result = calc(a, operator, b)
 
-function calc() {
-	getfirstNumber();
-	getsecondNumber();
+function calc(a, operator, b) {
+ if (operator == "+" ) {
+    return a + b;
+ }else if (operator == "-") {
+   return a - b;
+ }else if (operator == "*") {
+   return a * b;
+ }
+} 
+alert(`${a} ${operator} ${b} = ${result}`);
 
-switch (operator) {
-   case "+":
-      result = firstNumber + secondNumber;
-      alert(`Результат равен ${result}`);
-      break;
-   case "-":
-      result = firstNumber - secondNumber;
-      alert(`Результат равен ${result}`);
-      break;
-   case "*":
-      result = firstNumber * secondNumber;
-      alert(`Результат равен ${result}`);
-      break;
-      default:
-			alert(`Wrong characters!`);
-}
-}
-calc();
+// let firstNumber = parseFloat(prompt('Enter first number: '));
+// let operator = prompt('Enter operator ( either +, - or * ): ');
+// let secondNumber = parseFloat(prompt('Enter second number: '));
+// let result;
+
+// function calc() {
+// 	getfirstNumber();
+// 	getsecondNumber();
+
+// switch (operator) {
+//    case "+":
+//       result = firstNumber + secondNumber;
+//       alert(`Результат равен ${result}`);
+//       break;
+//    case "-":
+//       result = firstNumber - secondNumber;
+//       alert(`Результат равен ${result}`);
+//       break;
+//    case "*":
+//       result = firstNumber * secondNumber;
+//       alert(`Результат равен ${result}`);
+//       break;
+//       default:
+// 			alert(`Wrong characters!`);
+// }
+// }
+// calc();
---
+++ Tasks/obj.js
@@ -0,0 +1,6 @@
+let user = {
+    name: "John",
+    surname: Smith,
+};
+user[0] = John;
+console.log(user.name);
