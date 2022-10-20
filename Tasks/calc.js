86d169b77ba3055bcd8ec1ff064eff8b9b14219b
Kot-Kota <ses29091987@gmail.com>
2022-10-20 13:16:01 -0700

    new calc with obj

--- Tasks/calc.js
+++ Tasks/calc.js
@@ -1,58 +1,20 @@
-// function calc(operator, a, b){
-//    if (operator === "add") {
-//     return a+b;
-//    } else if (operator === "multi") {
-//     return a*b;
-//    } else if (operator === "sub") {
-//     return a-b;
-//    } else {
-//     return "Stop operator";
-//    }   
-// }
-// console.log(calc("add", 1, 2,))
-
+let operations = {
+    add: "+",
+    multi: "*",
+    sub: "-",
+}
 function calc(operator, a, b){
     if (typeof a === "string" || typeof b === "string")
      return "The Numbers is STR"
     switch (operator) {
-        case "add":
+        case operations.add:
           return (Number(a)+ Number(b))
-        case "multi":
+        case operations.multi:
           return a*b
-        case "sub":
+        case operations.sub:
           return a-b
           default:
             return "Stop operator";
       }
  }
- console.log(calc("sub", 1, 2,))
- 
-
-
-// let firstNumber = parseFloat(prompt('Enter first number: '));
-// let operator = prompt('Enter operator ( either +, - or * ): ');
-// let secondNumber = parseFloat(prompt('Enter second number: '));
-// let result;
-
-// function calc() {
-// 	getfirstNumber();
-// 	getsecondNumber();
-
-// switch (operator) {
-//    case "+":
-//       result = firstNumber + secondNumber;
-//       alert(`Результат равен ${result}`);
-//       break;
-//    case "-":
-//       result = firstNumber - secondNumber;
-//       alert(`Результат равен ${result}`);
-//       break;
-//    case "*":
-//       result = firstNumber * secondNumber;
-//       alert(`Результат равен ${result}`);
-//       break;
-//       default:
-// 			alert(`Wrong characters!`);
-// }
-// }
-// calc();
+ console.log(calc(operations.add, 1, 2,))
