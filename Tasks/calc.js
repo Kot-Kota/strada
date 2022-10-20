8fce634710ff6d0bfbf023027da2fa183bd0790b
Kot-Kota <ses29091987@gmail.com>
2022-10-20 12:49:10 -0700

    newcalc

--- Tasks/calc.js
+++ Tasks/calc.js
@@ -1,15 +1,31 @@
+// function calc(operator, a, b){
+//    if (operator === "add") {
+//     return a+b;
+//    } else if (operator === "multi") {
+//     return a*b;
+//    } else if (operator === "sub") {
+//     return a-b;
+//    } else {
+//     return "Stop operator";
+//    }   
+// }
+// console.log(calc("add", 1, 2,))
+
 function calc(operator, a, b){
-   if (operator === "add") {
-    return a+b;
-   } else if (operator === "multi") {
-    return a*b;
-   } else if (operator === "sub") {
-    return a-b;
-   } else {
-    return "Stop operator";
-   }   
-}
-console.log(calc("add", 1, 2,))
+    if (typeof a === "string" || typeof b === "string")
+     return "The Numbers is STR"
+    switch (operator) {
+        case "add":
+          return (Number(a)+ Number(b))
+        case "multi":
+          return a*b
+        case "sub":
+          return a-b
+          default:
+            return "Stop operator";
+      }
+ }
+ console.log(calc("sub", 1, 2,))
  
 
 
